e0ce73c69ff5cf611dfd4193729a273fbee10c57
printy6 <wh5938316@gmail.com>
2022-12-09 10:45:05 +0800

    fix: node imageType is error

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ npm install color-thief-ts
 
 ### example for browser
 
-``` typescript
+``` javascript
 import ColorThief from "color-thief-ts";
 
 const colorThief = new ColorThief();
@@ -28,14 +28,35 @@ console.log(palette);
 
 ### example for node.js
 
-``` typescript
+``` javascript
 import ColorThief from "color-thief-ts/node";
+import fetch from "node-fetch";
+import Sharp from "sharp";
 
 const colorThief = new ColorThief();
-const dominantColor = await colorThief.getColor("your-domaon/your-image-url.jpg");
-const palette = await colorThief.getPaletteAsync("your-domaon/your-image-url.jpg", 5);
 
-console.log(dominantColor);
-console.log(palette);
+const image = await fetch("http://localhost:3000/images/example.png")
+  .then((res) => res.arrayBuffer())
+  .then((arrayBuffer) => Buffer.from(arrayBuffer));
+
+const palette1 = await colorThief.getPalette(
+  { type: "image/png", buffer: image },
+  5
+);
+
+const palette2 = await colorThief.getPalette(
+  "http://localhost:3000/images/example.png",
+  5
+);
+
+const palette3 = await colorThief.getPalette(
+  "./images/example.png",
+  5
+);
+
+const color = await colorThief.getColor(
+  { type: "image/png", buffer: image }
+);
+
 ```
 
--- src/node.ts
+++ src/node.ts
@@ -4,18 +4,38 @@ import Core from "./core";
 import type { ColorArray, PaletteOptions } from "./type";
 import arrayToHex from "./utils/arrayToHex";
 
-type ImageType = any;
+/**
+ *
+ * image is the path to the file.
+ *  It can be
+ *  - a relative path
+ *  - an http url
+ *  - a data url
+ *  - an [in-memory Buffer](http://nodejs.org/api/buffer.html).
+ *
+ */
+type ImageType = string | { type: string; buffer: Buffer };
 
 class ColorThief extends Core {
   private getImageData(img: ImageType) {
     return new Promise<[ImageData, number]>((resolve, reject) => {
-      getPixels(img, function (err: any, data: any) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve([data, data.shape[0] * data.shape[1]]);
-        }
-      });
+      if (typeof img === "string") {
+        getPixels(img, function (err: any, data: any) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve([data, data.shape[0] * data.shape[1]]);
+          }
+        });
+      } else {
+        getPixels(img.buffer, img.type, function (err: any, data: any) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve([data, data.shape[0] * data.shape[1]]);
+          }
+        });
+      }
     });
   }
 
@@ -32,6 +52,8 @@ class ColorThief extends Core {
   ): Promise<string[]>;
 
   public getPalette(img: ImageType, colorCount = 10, opts?: PaletteOptions) {
+    const colorType = opts?.colorType ?? "hex";
+
     return new Promise((resolve, reject) => {
       this.getImageData(img)
         .then(([imageData, pixelCount]) => {
@@ -42,11 +64,11 @@ class ColorThief extends Core {
             opts
           );
 
-          if (opts?.colorType === "hex") {
-            resolve(palette.map((item) => arrayToHex(item)));
+          if (colorType === "hex") {
+            return resolve(palette.map((item) => arrayToHex(item)));
           }
 
-          resolve(palette);
+          return resolve(palette);
         })
         .catch((err) => {
           reject(err);
@@ -62,13 +84,15 @@ class ColorThief extends Core {
   public getColor(img: ImageType, opt?: PaletteOptions<"hex">): Promise<string>;
 
   public getColor(img: ImageType, opts?: PaletteOptions) {
+    const colorType = opts?.colorType ?? "hex";
+
     return new Promise((resolve, reject) => {
       this.getPalette(img, 5, {
         quality: opts?.quality ?? 10,
         colorType: "array",
       })
         .then((palette) => {
-          if (opts?.colorType === "hex") {
+          if (colorType === "hex") {
             return resolve(arrayToHex(palette[0]));
           }
 
